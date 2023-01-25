5633d27cdc5b24a370b53c01bbcceb432e8dd86e
MrGansoStr <ccounttnro1@gmail.com>
2023-01-24 23:44:40 -0500

    Box section

--- src/StyledComponents/RedirectBox/RedirectBox.jsx
+++ src/StyledComponents/RedirectBox/RedirectBox.jsx
@@ -5,14 +5,13 @@ function RedirectBox({ _height, _title, _info }) {
     height: _height
   }
   return (
-    <div className="border container d-flex align-items-center fustify-content-center p-3" style={style}>
-
+    <Box component="section" className="border container d-flex align-items-center fustify-content-center p-3" style={style}>
       <Box component="div" className="row">
         <Typography variant="h3">{_title}</Typography>
         <Typography varian="body1">{_info}</Typography>
       </Box>
 
-    </div>
+    </Box>
   );
 }
 export default RedirectBox;
