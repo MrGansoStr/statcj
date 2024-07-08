b3f89a6803150d75a671a79367930d4db8f21de7
Carlos David <104994361+MrGansoStr@users.noreply.github.com>
2024-07-08 18:12:43 -0500

    Change letter in Comments

--- src/pages/PublicPages/Comments/Comments.jsx
+++ src/pages/PublicPages/Comments/Comments.jsx
@@ -7,7 +7,7 @@ const BoxComments = lazy(() => import("./Components/BoxComments"));
 const Comments = memo(() => {
   return (
     <Box component="div" className="container-lg mt-5 mb-5">
-      <Typography variant="h4" className="text-center">Comentarios y Discuciones</Typography>
+      <Typography variant="h4" className="text-center">Comentarios y Discusiones</Typography>
       <Container maxWidth="md">
         <BoxComments/>
       </Container>
