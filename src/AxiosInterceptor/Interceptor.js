c0317f0475f9aa0f4273879812ad35b993768170
MrGansoStr <ccounttnro1@gmail.com>
2026-06-29 10:45:47 -0500

    chore(cleanup): remove console logs

--- src/AxiosInterceptor/Interceptor.js
+++ src/AxiosInterceptor/Interceptor.js
@@ -1,41 +1,42 @@
 import axios from "axios";
 import { SnackbarUtilities } from "../utilities/SnackbarManager";
-import { LSKeys } from './../models/LocalStorageKeys';
-import { GetLocalStorage } from './../utilities/ManageLocalStorage';
+import { LSKeys } from "./../models/LocalStorageKeys";
+import { GetLocalStorage } from "./../utilities/ManageLocalStorage";
 
 export const AxiosInterceptor = () => {
   const updateHeaderRecovery = (request) => {
     const tokenRecovery = GetLocalStorage(LSKeys.TOKENRECOVERY);
     const newHeaderRecovery = {
-      Authorization: `Bearer ${tokenRecovery}`
-    }
+      Authorization: `Bearer ${tokenRecovery}`,
+    };
     request.headers = newHeaderRecovery;
     return request;
-  }
+  };
   const updateHeaders = (request) => {
     const tokenJWT = GetLocalStorage(LSKeys.TOKEN);
     const newHeader = {
-      Authorization: `Bearer ${tokenJWT}`
-    }
+      Authorization: `Bearer ${tokenJWT}`,
+    };
     request.headers = newHeader;
     return request;
-  }
+  };
 
-  axios.interceptors.request.use(request => {
-    if (request?.url?.includes("/recoveryaccount")) return updateHeaderRecovery(request);
-    if(request?.url?.includes("/private-api")) return updateHeaders(request);
-    else if(request?.url?.includes("public-api")) return request;
+  axios.interceptors.request.use((request) => {
+    if (request?.url?.includes("/recoveryaccount"))
+      return updateHeaderRecovery(request);
+    if (request?.url?.includes("/private-api")) return updateHeaders(request);
+    else if (request?.url?.includes("public-api")) return request;
   });
 
-  axios.interceptors.response.use(response => {
-    //console.log(response?.data?.message);
-    console.log("Request Success");
-    SnackbarUtilities.success(response?.data?.message);
-    return response;
-  }, (error) => {
-    console.log(error);
-    SnackbarUtilities.error(error?.response?.data?.message);
-    console.log("Request Failed");
-    return Promise.reject(error);
-  })
-}
+  axios.interceptors.response.use(
+    (response) => {
+      SnackbarUtilities.success(response?.data?.message);
+      return response;
+    },
+    (error) => {
+      SnackbarUtilities.error(error?.response?.data?.message);
+      return Promise.reject(error);
+    },
+  );
+};
+
