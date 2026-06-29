668ff6cb2d16365a15a08cb83242c587f5b24f94
MrGansoStr <ccounttnro1@gmail.com>
2026-06-29 10:58:34 -0500

    chore(cleanup): add code-style without spacing

--- src/pages/Calculator/MCT/Components/MCT_calculator.jsx
+++ src/pages/Calculator/MCT/Components/MCT_calculator.jsx
@@ -1,30 +1,29 @@
 import { Stack, Box, Button, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { CalculateAllMCT } from "../../../Calculations/CalculationsMct";
-import { ProcessInput } from '../../../../utilities/ProcessInput';
+import { ProcessInput } from "../../../../utilities/ProcessInput";
 import StyledErrorInput from "../../../../StyledComponents/StyledErrorInput/StyledErrorInput";
 import { ThereErrors } from "../../../../utilities/ThereErrors";
 
-function MCT_calculator() {
-
+export default function MCT_calculator() {
   const [rawData, setRawData] = useState("");
   const [calculatedValues, setcalculatedValues] = useState(null);
   const [showError, setShowError] = useState(false);
+
   const HideError = (e) => {
     setShowError(false);
-  }
+  };
   const handleChangeRawData = (e) => {
     setRawData(e.target.value);
-  }
-
+  };
   const calculateMCT = (e) => {
-    if(ThereErrors(rawData)){
+    if (ThereErrors(rawData)) {
       setShowError(true);
-    }
-    else {
+    } else {
       setcalculatedValues(CalculateAllMCT(ProcessInput(rawData)));
     }
-  }
+  };
+
   return (
     <Stack component="div" spacing={3} className="d-flex align-items-center">
       <TextField
@@ -39,23 +38,32 @@ function MCT_calculator() {
         onChange={handleChangeRawData}
       />
       <Box component="span" className="">
-        <Button color="secondary" variant="outlined" onClick={calculateMCT}>Calcular</Button>
+        <Button color="secondary" variant="outlined" onClick={calculateMCT}>
+          Calcular
+        </Button>
       </Box>
-      {
-        showError ? <StyledErrorInput show={showError} HideError={HideError}/> : null
-      }
+      {showError ? (
+        <StyledErrorInput show={showError} HideError={HideError} />
+      ) : null}
       <Box component="div">
         <Typography component="p" variant="overline" className="fs-6">
-          Media: {calculatedValues ? <b>{calculatedValues.Media}</b> : "no Calculado"}
+          Media:{" "}
+          {calculatedValues ? <b>{calculatedValues.Media}</b> : "no Calculado"}
         </Typography>
         <Typography component="p" variant="overline" className="fs-6">
-          Moda: {calculatedValues ? <b>{calculatedValues.Moda}</b> : "no Calculado"}
+          Moda:{" "}
+          {calculatedValues ? <b>{calculatedValues.Moda}</b> : "no Calculado"}
         </Typography>
         <Typography component="p" variant="overline" className="fs-6">
-          Mediana: {calculatedValues ? <b>{calculatedValues.Mediana}</b> : "no Calculado"}
+          Mediana:{" "}
+          {calculatedValues ? (
+            <b>{calculatedValues.Mediana}</b>
+          ) : (
+            "No calculado"
+          )}
         </Typography>
       </Box>
     </Stack>
   );
 }
-export default MCT_calculator;
+
